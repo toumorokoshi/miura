f4c2d3e55e15b72c954007bf284a8f694fa40857
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-06-25 01:11:35 -0700

    test_utils has 100% coverage

--- miura/tests/test_utils.py
+++ miura/tests/test_utils.py
@@ -14,3 +14,16 @@ class TestSpecializeContent(object):
             job_data
         )
         assert output == "foojob, hello world"
+
+
+def test_format_path_to_module():
+    test_path = "./foo.py"
+    assert utils.format_path_to_module(test_path) == "foo.py"
+
+
+def test_get_method_from_module():
+    method = utils.get_method_from_module(
+        'miura.utils',
+        'format_path_to_module'
+    )
+    assert method is utils.format_path_to_module
