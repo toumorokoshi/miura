23cf3bbc13e7e257b0c37ad9874b28c4827068a5
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-11-20 17:56:41 -0800

    pushing new miura

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(name='miura',
-      version='0.1.0',
+      version='0.1.3',
       description='a Jenkins job management tool',
       long_description=open('README.rst').read(),
       author='Yusuke Tsutsumi',
